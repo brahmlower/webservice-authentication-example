2ba494aad254cb4c8d333adacaa2a0b74ede234a
Brahm Lower <bplower@gmail.com>
2019-03-20 00:26:15 -0700

    Added building graphic to vcs

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,35 +1,15 @@
 import React, { Component } from 'react'
-// import { Link } from 'react-router-dom'
 import { Jumbotron, Container, Button, Nav, Navbar } from 'react-bootstrap';
-// import { linkButtonStyle } from '../Common.js';
 import './Landing.css';
 
 // https://www.kisspng.com/png-high-rise-building-skyscraper-vector-skyscrapers-95811/
-const buildingImageUrl = "https://png2.kisspng.com/sh/7b46047055b586d6d02374c1aaf11601/L0KzQYm3UcI5N6luj5H0aYP2gLBuTfhqb5kyitt8ZT3lhbrzhPlvb154gAt8Y4LkgLb5TgZma6V0ip98a4n2c8PokPVze146eahuMUS5RbOCUBU6PF83TqsCM0K0SYK8UccyPmQ7SqMCNUi7PsH1h5==/kisspng-high-rise-building-skyscraper-vector-skyscrapers-5a6e1465b93e94.2697321915171636217588.png"
-// const checkerImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Checkermotoslogo.svg/240px-Checkermotoslogo.svg.png"
-const buildingJsonUrl = "/buildings_json.png"
-// const loginLink = (
-//   <Link to="/login">
-//     <Button variant="link" style={linkButtonStyle}>
-//       Login
-//     </Button>
-//   </Link>
-// )
-
-// const signupLink = (
-//   <Link to="/signup">
-//     <Button variant="link" style={linkButtonStyle}>
-//       Sign Up
-//     </Button>
-//   </Link>
-// )
 
 class InteractiveGraphic extends Component {
   render () {
     return (
       <div className="jumbotronImg">
-        <img src={buildingJsonUrl} height="400" withd="400" id="codeImg" alt="black and white checker pattern" />
-        <img src={buildingImageUrl} height="400" width="400" id="buildingImg" alt="vector art of building" />
+        <img src="/buildings_json.png" height="400" withd="400" id="codeImg" alt="building json screenshot" />
+        <img src="/buildings_graphic.png" height="400" width="400" id="buildingImg" alt="vector art of building" />
       </div>
     )
   }
